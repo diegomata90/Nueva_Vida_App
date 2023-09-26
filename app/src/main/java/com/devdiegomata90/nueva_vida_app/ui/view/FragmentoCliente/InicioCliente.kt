9bbcd978fae00aeca4a0e83298f49e30891ae1f0
diegomata90 <diegomata90@gmail.com>
2023-09-25 19:05:12 -0600

    Crea menu InicioCliente.kt

--- app/src/main/java/com/devdiegomata90/nueva_vida_app/ui/view/FragmentoCliente/InicioCliente.kt
+++ app/src/main/java/com/devdiegomata90/nueva_vida_app/ui/view/FragmentoCliente/InicioCliente.kt
@@ -1,11 +1,14 @@
 package com.devdiegomata90.nueva_vida_app.ui.view.FragmentoCliente
 
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.devdiegomata90.nueva_vida_app.R
@@ -18,35 +21,69 @@ class InicioCliente : Fragment() {
     private lateinit var rvCategorias: RecyclerView
     private lateinit var categoriasAdapter: CategoriasAdapter
     private lateinit var databaseReference: DatabaseReference
+    private lateinit var CategoriasTXT: TextView
+    private lateinit var OtrasCategoriasTXT: TextView
+    private lateinit var Evento:TextView
+    private lateinit var Biblia:TextView
+    private lateinit var Audio:TextView
+    private lateinit var Video:TextView
+    private lateinit var cardEvento:CardView
+    private lateinit var cardBiblia:CardView
+    private lateinit var cardAudio:CardView
+    private lateinit var cardVideo:CardView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //inflar la vista
+        val view = inflater.inflate(R.layout.fragment_inicio_cliente, container, false)
 
-        //Crear tipo letra personalizado
-        //val typefaceUbuntu = Typeface.createFromAsset(context?.assets,"fons/Ubuntu.ttf")
-        //val inicio_cliente = view.findViewById<TextView>(R.id.inicio_cliente)
-        //inicio_cliente.typeface = typefaceUbuntu
+        initComponent(view)
+        initUi(view)
+        eventos()
 
-        return inflater.inflate(R.layout.fragment_inicio_cliente, container, false)
+
+        // Crear tipo letra personalizado (Ubuntu)
+        val typefaceUbuntu = Typeface.createFromAsset(requireContext().assets, "fons/Ubuntu.ttf")
+
+
+        // Aplicar la fuente Ubuntu a los elementos de texto
+        CategoriasTXT.typeface = typefaceUbuntu
+        OtrasCategoriasTXT.typeface = typefaceUbuntu
+        Evento.typeface = typefaceUbuntu
+        Biblia.typeface = typefaceUbuntu
+        Audio.typeface = typefaceUbuntu
+        Video.typeface = typefaceUbuntu
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initComponent(view)
-        initUi(view)
     }
 
     private fun initComponent(view: View) {
         rvCategorias = view.findViewById(R.id.rvCategorias)
         databaseReference = FirebaseDatabase.getInstance().getReference("Categorias")
+        CategoriasTXT = view.findViewById(R.id.CategoriasTXT)
+        OtrasCategoriasTXT = view.findViewById(R.id.OtrasCategoriasTXT)
+        Evento = view.findViewById(R.id.Evento)
+        Biblia = view.findViewById(R.id.Biblia)
+        Audio = view.findViewById(R.id.Audio)
+        Video = view.findViewById(R.id.Video)
+        cardEvento = view.findViewById(R.id.cardEvento)
+        cardBiblia = view.findViewById(R.id.cardBiblia)
+        cardAudio = view.findViewById(R.id.cardAudio)
+        cardVideo = view.findViewById(R.id.cardVideo)
+
     }
 
     private fun initUi(view: View) {
 
-        val query =databaseReference.orderByChild("nombre") // Ordenar por el campo "nombre" si es necesario
+        val query =
+            databaseReference.orderByChild("nombre") // Ordenar por el campo "nombre" si es necesario
         val categoriaList = ArrayList<Categoria>()
 
         query.addValueEventListener(object : ValueEventListener {
@@ -68,9 +105,31 @@ class InicioCliente : Fragment() {
 
             override fun onCancelled(databaseError: DatabaseError) {
                 // Manejar errores si es necesario
-                Toast.makeText(requireContext(), "Error en Firebase: ${databaseError.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    requireContext(),
+                    "Error en Firebase: ${databaseError.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         })
+
+    }
+
+    private fun eventos() {
+        cardEvento.setOnClickListener{
+            Toast.makeText(requireContext(),"Ya casi esta listo la EVENTO, porfavor esperar",Toast.LENGTH_SHORT).show()
+        }
+        cardBiblia.setOnClickListener {
+            Toast.makeText(requireContext(),"Ya casi esta listo la BIBLIA, porfavor esperar",Toast.LENGTH_SHORT).show()
+        }
+        cardAudio.setOnClickListener {
+            Toast.makeText(requireContext(),"Ya casi esta listo la AUDIO, porfavor esperar",Toast.LENGTH_SHORT).show()
+        }
+        cardVideo.setOnClickListener {
+            Toast.makeText(requireContext(),"Ya casi esta listo la VIDEO, porfavor esperar",Toast.LENGTH_SHORT).show()
+        }
+
+
 
     }
 
