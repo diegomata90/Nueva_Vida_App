11debed2a81f143c15df17b161a0c279e8368830
diegomata90 <diegomata90@gmail.com>
2023-12-04 16:03:04 -0600

    Se modifica codigo para que salga correctamente el versiculo del dia

--- app/src/main/java/com/devdiegomata90/nueva_vida_app/domain/GetDailyVerseUseCase.kt
+++ app/src/main/java/com/devdiegomata90/nueva_vida_app/domain/GetDailyVerseUseCase.kt
@@ -4,6 +4,7 @@ import com.devdiegomata90.nueva_vida_app.data.model.DailyVerseResponse
 import com.devdiegomata90.nueva_vida_app.data.repository.DailyVerseRepository
 import java.util.*
 
+
 class GetDailyVerseUseCase {
 
     private val repository = DailyVerseRepository()
@@ -12,17 +13,24 @@ class GetDailyVerseUseCase {
         //Obtener la fecha de hoy
         val calendar = Calendar.getInstance()
 
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val day: String = if (calendar.get(Calendar.DAY_OF_MONTH) < 10) {
+            "0${calendar.get(Calendar.DAY_OF_MONTH)}"
+        } else {
+            "${calendar.get(Calendar.DAY_OF_MONTH)}"
+        }
 
-        val month = calendar.get(Calendar.MONTH)+1
-
+        val month = if (calendar.get(Calendar.MONTH) < 9) {
+            "0${calendar.get(Calendar.MONTH) + 1}"
+        } else {
+            calendar.get(Calendar.MONTH) + 1
+        }
 
         //Forma el URL
         val Url = "$day-$month.json"
 
         val dailyVerse = repository.getDailyVerse(Url)
 
-        if(dailyVerse != null){
+        if (dailyVerse != null) {
             //devuelve
             return dailyVerse
         }
