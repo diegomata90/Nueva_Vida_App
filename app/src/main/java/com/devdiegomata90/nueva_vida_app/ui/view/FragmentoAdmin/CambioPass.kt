cc7f1e91c514f3e6aa62738193f06131f85c7338
diegomata90 <diegomata90@gmail.com>
2024-02-14 15:11:44 -0600

    Se agregar el diseño para el cambio de contraseña

---
+++ app/src/main/java/com/devdiegomata90/nueva_vida_app/ui/view/FragmentoAdmin/CambioPass.kt
@@ -0,0 +1,12 @@
+package com.devdiegomata90.nueva_vida_app.ui.view.FragmentoAdmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devdiegomata90.nueva_vida_app.R
+
+class CambioPass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cambio_pass)
+    }
+}
--- app/src/main/java/com/devdiegomata90/nueva_vida_app/ui/view/FragmentoAdmin/PerfilAdmin.kt
+++ app/src/main/java/com/devdiegomata90/nueva_vida_app/ui/view/FragmentoAdmin/PerfilAdmin.kt
@@ -1,13 +1,18 @@
 package com.devdiegomata90.nueva_vida_app.ui.view.FragmentoAdmin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.devdiegomata90.nueva_vida_app.R
 
+
 class PerfilAdmin : Fragment() {
+
+    private lateinit var cambioPass: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,8 +21,24 @@ class PerfilAdmin : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_perfil_admin, container, false)
 
+        initComponent(view)
+        eventos()
 
         return view
+    }
+
+    fun initComponent(view: View) {
+
+        cambioPass = view.findViewById(R.id.CambioPass)
+
+    }
+
+    fun eventos(){
+
+        cambioPass.setOnClickListener {
+
+            startActivity(Intent(requireContext(), CambioPass::class.java))
+        }
     }
 
 }
